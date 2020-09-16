043686970a3ac26310bf08a1b6ff18b1e6ec11f5
sarakluge <Paddel2012>
2020-09-16 09:32:10 +0200

    Added todo-list

--- app/src/main/java/com/example/languagelocalization/MainActivity.kt
+++ app/src/main/java/com/example/languagelocalization/MainActivity.kt
@@ -4,9 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
     }
+
+
+
 }
+
+
+//Anpassa appen till svenska och engelska
+//1. anpassa xml - layout
+//2. anpassa kotlin-fil
+//3. anpassa bilder

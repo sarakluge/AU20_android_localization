8529b0c3bd567e2362bc404d509ae8ddc43a8582
sarakluge <Paddel2012>
2020-09-16 10:46:41 +0200

    added bottom that changes text

--- app/src/main/java/com/example/languagelocalization/MainActivity.kt
+++ app/src/main/java/com/example/languagelocalization/MainActivity.kt
@@ -2,16 +2,25 @@ package com.example.languagelocalization
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var greetingTextView: TextView
+    var name = "Sara"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        greetingTextView = findViewById(R.id.textView)
     }
 
-
+    fun buttonPress(view: View) {
+        greetingTextView.text = getString(R.string.great, name)
+    }
 
 }
 
